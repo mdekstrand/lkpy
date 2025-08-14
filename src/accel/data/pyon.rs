fd532785d320232ec2d5ca3493cb18ce29c87033
Michael Ekstrand <mdekstrand@drexel.edu>
2025-08-14 17:22:00 -0400

    Revert "try using python-parser instead of rustparser-python"
    This reverts commit 89948eb0d42acd6b180dadd89247001b14e7dd5b.

--- src/accel/data/pyon.rs
+++ src/accel/data/pyon.rs
@@ -11,71 +11,26 @@ use std::any::Any;
 use pyo3::{
     exceptions::{PyTypeError, PyValueError},
     prelude::*,
-    types::{PyBool, PyDict, PyFloat, PyInt, PyList, PyNone, PyString},
+    types::{PyBool, PyComplex, PyDict, PyFloat, PyInt, PyList, PyNone, PyString},
 };
 
-use python_parser::{ast::SetItem, eval_input};
-use python_parser::{
-    ast::{DictItem, Expression},
-    make_strspan,
-};
+use rustpython_ast::{Constant, Expr, ExprDict, ExprList};
+use rustpython_parser::Parse;
 
-/// Parse a “pyon” object.
+/// Parse a “pyson” object.
 #[pyfunction]
-pub fn pyon_loads<'py>(py: Python<'py>, text: &str) -> PyResult<PyObject> {
-    let (_r, exprs) = eval_input(make_strspan(text))
+pub fn pyson_loads<'py>(py: Python<'py>, text: &str) -> PyResult<PyObject> {
+    let ast = ExprDict::parse(text, "internal")
         .map_err(|_e| PyErr::new::<PyValueError, _>("Python parse error"))?;
-    if exprs.len() != 1 {
-        Err(PyValueError::new_err(format!(
-            "parsed {} expressions, expected 1",
-            exprs.len()
-        )))
-    } else {
-        let obj = realize_value(py, exprs.into_iter().next().unwrap())?;
-        Ok(obj.unbind())
-    }
+    let obj = realize_dict(py, ast)?;
+    Ok(obj.unbind())
 }
 
-fn realize_value<'py>(py: Python<'py>, ast: Expression) -> PyResult<Bound<'py, PyAny>> {
+fn realize_value<'py>(py: Python<'py>, ast: Expr) -> PyResult<Bound<'py, PyAny>> {
     match ast {
-        Expression::None => Ok(PyNone::get(py).to_owned().into_any()),
-        Expression::True => Ok(PyBool::new(py, true).to_owned().into_any()),
-        Expression::False => Ok(PyBool::new(py, false).to_owned().into_any()),
-        Expression::Bytes(val) => Ok(PyString::new(py, str::from_utf8(&val)?).into_any()),
-        Expression::String(chunks) => {
-            let string: String = chunks.into_iter().map(|c| c.content).collect();
-            Ok(PyString::new(py, &string).into_any())
-        }
-        Expression::Int(n) => Ok(PyInt::new(py, n).into_any()),
-        Expression::Float(x) => Ok(PyFloat::new(py, x).into_any()),
-        Expression::Ellipsis => Err(PyErr::new::<PyValueError, _>("ellipsis not supported")),
-        Expression::ListLiteral(items) => {
-            let out = PyList::empty(py);
-
-            for elt in items {
-                if let SetItem::Unique(e) = elt {
-                    out.append(realize_value(py, e)?)?;
-                } else {
-                    return Err(PyValueError::new_err("splat not supported"));
-                }
-            }
-
-            Ok(out.into_any())
-        }
-        Expression::DictLiteral(items) => {
-            let out = PyDict::new(py);
-
-            for elt in items {
-                if let DictItem::Unique(name, value) = elt {
-                    let name = expect_string(name)?;
-                    out.set_item(name, realize_value(py, value)?)?;
-                } else {
-                    return Err(PyValueError::new_err("splat not supported"));
-                }
-            }
-
-            Ok(out.into_any())
-        }
+        Expr::Constant(c) => realize_constant(py, c.value),
+        Expr::List(list) => realize_list(py, list),
+        Expr::Dict(dict) => realize_dict(py, dict),
         _ => Err(PyErr::new::<PyValueError, _>(format!(
             "unsupported expression type {:?}",
             ast.type_id()
@@ -83,13 +38,70 @@ fn realize_value<'py>(py: Python<'py>, ast: Expression) -> PyResult<Bound<'py, P
     }
 }
 
-fn expect_string(val: Expression) -> PyResult<String> {
-    match val {
-        Expression::String(s) => Ok(s.into_iter().map(|c| c.content).collect()),
-        Expression::Bytes(s) => Ok(String::from_utf8(s)?),
-        _ => Err(PyErr::new::<PyTypeError, _>(format!(
-            "expected string, got {:?}",
-            val
-        ))),
+fn realize_constant<'py>(py: Python<'py>, c: Constant) -> PyResult<Bound<'py, PyAny>> {
+    match c {
+        Constant::Bool(val) => Ok(PyBool::new(py, val).to_owned().into_any()),
+        Constant::Bytes(val) => Ok(PyString::new(py, str::from_utf8(&val)?).into_any()),
+        Constant::None => Ok(PyNone::get(py).to_owned().into_any()),
+        Constant::Str(s) => Ok(PyString::new(py, &s).into_any()),
+        Constant::Int(big_int) => {
+            let n: i64 = big_int
+                .try_into()
+                .map_err(|_e| PyErr::new::<PyValueError, _>("integer out of bounds"))?;
+            Ok(PyInt::new(py, n).into_any())
+        }
+        Constant::Tuple(constants) => {
+            let list = PyList::empty(py);
+            for elt in constants {
+                list.append(realize_constant(py, elt)?)?;
+            }
+            Ok(list.into_any())
+        }
+        Constant::Float(x) => Ok(PyFloat::new(py, x).into_any()),
+        Constant::Complex { real, imag } => Ok(PyComplex::from_doubles(py, real, imag).into_any()),
+        Constant::Ellipsis => Err(PyErr::new::<PyValueError, _>("ellipsis not supported")),
+    }
+}
+
+fn realize_list<'py>(py: Python<'py>, list: ExprList) -> PyResult<Bound<'py, PyAny>> {
+    let out = PyList::empty(py);
+
+    for elt in list.elts {
+        out.append(realize_value(py, elt)?)?;
+    }
+
+    Ok(out.into_any())
+}
+
+fn realize_dict<'py>(py: Python<'py>, dict: ExprDict) -> PyResult<Bound<'py, PyAny>> {
+    let out = PyDict::new(py);
+
+    for (key, val) in dict.keys.into_iter().zip(dict.values.into_iter()) {
+        if let Some(key) = key {
+            let key = expect_string(key)?;
+            let val = realize_value(py, val)?;
+            out.set_item(key, val)?;
+        } else {
+            return Err(PyErr::new::<PyValueError, _>(
+                "dictionary splat not supported",
+            ));
+        }
+    }
+
+    Ok(out.into_any())
+}
+
+fn expect_string(val: Expr) -> PyResult<String> {
+    if let Some(c) = val.constant_expr() {
+        match c.value {
+            Constant::Str(s) => Ok(s),
+            Constant::Bytes(s) => Ok(String::from_utf8(s)?),
+            _ => Err(PyErr::new::<PyTypeError, _>(format!(
+                "expected string, got {:?}",
+                c.kind
+            ))),
+        }
+    } else {
+        Err(PyErr::new::<PyValueError, _>("expression is not constant"))
     }
 }
